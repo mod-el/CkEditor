0ea74f38472943c93ee51c7c58797faa448a3b5f
Domenico Giambra <sviluppo@editt.it>
2017-12-02 00:33:54 +0100

    Changed obeserver behaviour

--- files/check.js
+++ files/check.js
@@ -1,61 +1,56 @@
 var ckeditorsArr = [];
 
 function checkCkEditor() {
-	if (typeof CKEDITOR === 'undefined')
-		return false;
+	return new Promise(function(resolve) {
+		if (typeof CKEDITOR === 'undefined')
+			return resolve();
 
-	var elements = document.querySelectorAll('.ckeditor_textarea');
-	for (var i in elements) {
-		if (!elements.hasOwnProperty(i)) continue;
-		if (elements[i].offsetParent === null) continue;
-		if (elements[i].getAttribute('data-ckeditor-attached')!==null) continue;
+		var elements = document.querySelectorAll('.ckeditor_textarea');
+		for (var i in elements) {
+			if (!elements.hasOwnProperty(i)) continue;
+			if (elements[i].offsetParent === null) continue;
+			if (elements[i].getAttribute('data-ckeditor-attached') !== null) continue;
 
-		var options = {
-			'extraPlugins': 'uploadimage',
-			'uploadUrl': base_path+'model/CkEditor/files/upload.php?type=drop',
-			'filebrowserUploadUrl': base_path+'model/CkEditor/files/upload.php?type=upload',
-			'skin': 'moonocolor,'+base_path+'model/CkEditor/files/skins/moonocolor/'
-		};
-		if (elements[i].getAttribute('data-ckeditor')) {
-			var newOptions = JSON.parse(elements[i].getAttribute('data-ckeditor'));
-			if(typeof newOptions==='object'){
-				for(var k in newOptions){
-					options[k] = newOptions[k];
+			var options = {
+				'extraPlugins': 'uploadimage',
+				'uploadUrl': base_path + 'model/CkEditor/files/upload.php?type=drop',
+				'filebrowserUploadUrl': base_path + 'model/CkEditor/files/upload.php?type=upload',
+				'skin': 'moonocolor,' + base_path + 'model/CkEditor/files/skins/moonocolor/'
+			};
+			if (elements[i].getAttribute('data-ckeditor')) {
+				var newOptions = JSON.parse(elements[i].getAttribute('data-ckeditor'));
+				if (typeof newOptions === 'object') {
+					for (var k in newOptions) {
+						options[k] = newOptions[k];
+					}
 				}
 			}
+
+			var editor = CKEDITOR.replace(elements[i], options);
+			var index = (ckeditorsArr.push(editor)) - 1;
+
+			elements[i].setAttribute('data-getvalue-function', 'getCkEditorValue');
+			elements[i].setAttribute('data-setvalue-function', 'setCkEditorValue');
+			elements[i].setAttribute('data-ckeditor-attached', index);
+
+			editor.on('blur', (function (el) {
+				return function () {
+					if ("createEvent" in document) {
+						var evt = document.createEvent("HTMLEvents");
+						evt.initEvent("change", false, true);
+						el.dispatchEvent(evt);
+					} else {
+						el.fireEvent("onchange");
+					}
+				}
+			})(elements[i]));
 		}
-
-		var editor = CKEDITOR.replace(elements[i], options);
-		var index = (ckeditorsArr.push(editor))-1;
-
-		elements[i].setAttribute('data-getvalue-function', 'getCkEditorValue');
-		elements[i].setAttribute('data-setvalue-function', 'setCkEditorValue');
-		elements[i].setAttribute('data-ckeditor-attached', index);
-
-		editor.on('blur', (function(el){
-			return function(){
-				if ("createEvent" in document) {
-					var evt = document.createEvent("HTMLEvents");
-					evt.initEvent("change", false, true);
-					el.dispatchEvent(evt);
-				}else{
-					el.fireEvent("onchange");
-				}
-			}
-		})(elements[i]));
-	}
+		resolve();
+	});
 }
 
 window.addEventListener('load', function(){
-	checkCkEditor();
-
-	if (typeof MutationObserver !== 'undefined') {
-		var observer = new MutationObserver(function (mutations) {
-			checkCkEditor();
-		});
-
-		observer.observe(document.body, {"childList": true, "subtree": true});
-	}
+	observeMutations(checkCkEditor);
 });
 
 function getCkEditorValue(){
