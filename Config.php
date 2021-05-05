3efd0a5a227a6912c2066c746e1e62dcf17a6f22
Domenico Giambra <zorinik@gmail.com>
2021-05-05 14:40:18 +0200

    Aggiornato CkEditor e rimodulata config

--- Config.php
+++ Config.php
@@ -11,63 +11,81 @@ class Config extends Module_Config
 		$this->addAsset('config', 'config.php', function () {
 			return '<?php
 $config = ' . var_export([
-					'removePlugins' => 'save',
-					'toolbarGroups' => [
-						[
-							'name' => 'document',
-							'groups' => ['document', 'doctools', 'mode'],
-						],
-						[
-							'name' => 'clipboard',
-							'groups' => ['clipboard', 'undo'],
-						],
-						[
-							'name' => 'editing',
-							'groups' => ['find', 'selection', 'spellchecker', 'editing'],
-						],
-						[
-							'name' => 'styles',
-							'groups' => ['styles'],
-						],
-						[
-							'name' => 'forms',
-							'groups' => ['forms'],
-						],
-						[
-							'name' => 'links',
-							'groups' => ['links'],
-						],
-						[
-							'name' => 'tools',
-							'groups' => ['tools'],
-						],
-						'/',
-						[
-							'name' => 'basicstyles',
-							'groups' => ['basicstyles', 'cleanup'],
-						],
-						[
-							'name' => 'colors',
-							'groups' => ['colors'],
-						],
-						[
-							'name' => 'paragraph',
-							'groups' => ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'],
-						],
-						[
-							'name' => 'insert',
-							'groups' => ['insert'],
-						],
-						[
-							'name' => 'others',
-							'groups' => ['others'],
-						],
-						[
-							'name' => 'about',
-							'groups' => ['about'],
+					'include-host-in-uploads' => false,
+					'ckeditor' => [
+						'removePlugins' => 'save',
+						'toolbarGroups' => [
+							[
+								'name' => 'document',
+								'groups' => ['document', 'doctools', 'mode'],
+							],
+							[
+								'name' => 'clipboard',
+								'groups' => ['clipboard', 'undo'],
+							],
+							[
+								'name' => 'editing',
+								'groups' => ['find', 'selection', 'spellchecker', 'editing'],
+							],
+							[
+								'name' => 'styles',
+								'groups' => ['styles'],
+							],
+							[
+								'name' => 'forms',
+								'groups' => ['forms'],
+							],
+							[
+								'name' => 'links',
+								'groups' => ['links'],
+							],
+							[
+								'name' => 'tools',
+								'groups' => ['tools'],
+							],
+							'/',
+							[
+								'name' => 'basicstyles',
+								'groups' => ['basicstyles', 'cleanup'],
+							],
+							[
+								'name' => 'colors',
+								'groups' => ['colors'],
+							],
+							[
+								'name' => 'paragraph',
+								'groups' => ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'],
+							],
+							[
+								'name' => 'insert',
+								'groups' => ['insert'],
+							],
+							[
+								'name' => 'others',
+								'groups' => ['others'],
+							],
+							[
+								'name' => 'about',
+								'groups' => ['about'],
+							],
 						],
 					],
 				], true) . ";\n";
 		});
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function postUpdate_2_0_0()
+	{
+		$file = INCLUDE_PATH . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'User';
+		require_once(INCLUDE_PATH . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'CkEditor' . DIRECTORY_SEPARATOR . 'config.php');
+		$config = [
+			'include-host-in-uploads' => false,
+			'ckeditor' => $config,
+		];
+		file_put_contents($file, "<?php\n\$config = " . var_export($config, true) . ";\n");
+		return true;
 	}
 }
--- files/upload.php
+++ files/upload.php
@@ -1,48 +1,61 @@
 <?php
-require_once('..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+require_once('..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
+require_once('..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'CkEditor' . DIRECTORY_SEPARATOR . 'config.php');
 
 $url = null;
 
-if(isset($_FILES['upload'])){
+if (isset($_FILES['upload'])) {
 	$path = '../../../app-data/img/uploads/';
-	if(!is_dir($path))
+	if (!is_dir($path))
 		mkdir($path, 0777, true);
 
-	if($_FILES['upload']['error'] == UPLOAD_ERR_OK and is_uploaded_file($_FILES['upload']['tmp_name'])){
+	if ($_FILES['upload']['error'] == UPLOAD_ERR_OK and is_uploaded_file($_FILES['upload']['tmp_name'])) {
 		$ext = pathinfo($_FILES['upload']['name'], PATHINFO_EXTENSION);
 
-		do{
+		do {
 			$filename = mt_rand();
-		}while(file_exists($path.$filename.'.'.$ext));
+		} while (file_exists($path . $filename . '.' . $ext));
 
-		if(move_uploaded_file($_FILES['upload']['tmp_name'], $path.$filename.'.'.$ext)){
-			$url = PATH.'app-data/img/uploads/'.$filename.'.'.$ext;
-		}else{
+		if (move_uploaded_file($_FILES['upload']['tmp_name'], $path . $filename . '.' . $ext)) {
+			$host = '';
+			if ($config['include-host-in-uploads']) {
+				if (!defined('HTTPS')) {
+					if ((!empty($_SERVER['HTTPS']) and $_SERVER['HTTPS'] !== 'off') or ($_SERVER['SERVER_PORT'] ?? null) == 443)
+						define('HTTPS', 1);
+					else
+						define('HTTPS', 0);
+				}
+
+				$host = (HTTPS ? 'https' : 'http') . '://' . ($_SERVER['HTTP_HOST'] ?? '');
+			}
+
+			$url = $host . PATH . 'app-data/img/uploads/' . $filename . '.' . $ext;
+		} else {
 			$error = 'Error in saving file, please retry';
 		}
-	}else{
-		$error = 'Error '.$_FILES['upload']['error'].' in uploading file, please retry';
+	} else {
+		$error = 'Error ' . $_FILES['upload']['error'] . ' in uploading file, please retry';
 	}
-}else{
+} else {
 	$error = 'No file detected';
 }
 
-switch($_GET['type']){
+switch ($_GET['type']) {
 	case 'drop':
-		if(isset($error)){
+		if (isset($error)) {
 			$response = [
 				'uploaded' => 0,
 				'error' => [
 					'message' => $error,
 				],
 			];
-		}elseif(isset($url, $filename)){
+		} elseif (isset($url, $filename)) {
 			$response = [
 				'uploaded' => 1,
-				'fileName' => $filename.'.'.$ext,
+				'fileName' => $filename . '.' . $ext,
 				'url' => $url,
 			];
-		}else{
+		} else {
 			$response = [
 				'uploaded' => 0,
 				'error' => [
